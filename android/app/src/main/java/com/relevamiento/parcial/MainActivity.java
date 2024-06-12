bff096b12ea1f98afc4a264fe65dd03c045ad672
JuanPabloDH <juanpid29@gmail.com>
2024-06-12 19:43:09 -0300

    borrar proyecto-module

--- android/app/src/main/java/com/relevamiento/parcial/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.segundo.parcial;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.segundo.parcial',
-  appName: 'SP Restaurante',
-  webDir: 'www'
-};
-
-export default config;
