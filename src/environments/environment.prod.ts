2879e4798739f9a5032278cb28b1b04c790d2aa3
JuanPabloDH <juanpid29@gmail.com>
2024-06-13 08:01:02 -0300

    arreglar error de environment.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCbu5bnEd446epckBSs4i8XPvw-aWwz4tk', //OK
+    authDomain: 'pps-segundo-parcial-95514.appspot.com', //OK
+    projectId: 'pps-segundo-parcial-95514', //OK
+    storageBucket: 'pps-segundo-parcial-95514.appspot.com', //OK
+    messagingSenderId: '800945683480', //OK
+    appId: '1:800945683480:android:ff123b19a84933f3b4576d', //OK
+  },
 };
