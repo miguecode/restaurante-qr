254cd36a16f4841f4946d07eaf38b868c01d0050
JuanPabloDH <juanpid29@gmail.com>
2024-06-12 22:35:22 -0300

    pusheo a mi rama

---
+++ src/app/pipes/highlight.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'highlight',
+  standalone: true
+})
+export class HighlightPipe implements PipeTransform {
+
+  transform(value: unknown, ...args: unknown[]): unknown {
+    return null;
+  }
+
+}
