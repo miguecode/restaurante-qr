802a2ee1512113fd1e23c31e3dc162a143a043c7
JuanPabloDH <juanpid29@gmail.com>
2024-06-13 09:58:27 -0300

    servicios de auth y firestore

--- src/app/services/firebase/auth.service.ts
+++ src/app/services/firebase/auth.service.ts
@@ -1,9 +1,65 @@
 import { Injectable } from '@angular/core';
+import {
+  Auth,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from '@angular/fire/auth';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private credenciales: any = undefined;
 
-  constructor() { }
+  constructor(private auth: Auth) {
+    onAuthStateChanged(this.auth, (userCred) => {
+      if (userCred) {
+        this.credenciales = userCred;
+      }
+    });
+  }
+
+  public iniciarSesion(correo: string, clave: string) {
+    return signInWithEmailAndPassword(this.auth, correo, clave);
+  }
+
+  public registrar(correo: string, clave: string) {
+    return createUserWithEmailAndPassword(this.auth, correo, clave);
+  }
+
+  public getCorreo() {
+    return new Promise<string | undefined>((ok) => {
+      if (this.credenciales) {
+        ok(this.credenciales.email);
+      } else {
+        setTimeout(() => {
+          if (!this.credenciales) {
+            ok(undefined);
+          }
+          ok(this.credenciales.email);
+        }, 500);
+      }
+    });
+  }
+
+  public getVerificoCorreo() {
+    return new Promise<boolean | undefined>((ok) => {
+      if (this.credenciales) {
+        ok(this.credenciales.emailVerified);
+      } else {
+        setTimeout(() => {
+          if (!this.credenciales) {
+            ok(undefined);
+          }
+          ok(this.credenciales.emailVerified);
+        }, 500);
+      }
+    });
+  }
+
+  public cerrarSesion() {
+    return signOut(this.auth);
+  }
 }
