161e523cecac1c8e96c3641157eb4dfe1dabe6e6
santiifernandez88 <santiifernandez88@gmail.com>
2024-06-20 20:58:41 -0300

    alta clientes check

--- src/app/components/shared/formulario-cliente/formulario-cliente.component.ts
+++ src/app/components/shared/formulario-cliente/formulario-cliente.component.ts
@@ -1,15 +1,166 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+import { Camera, CameraResultType } from '@capacitor/camera';
+import { Swalert } from 'src/app/classes/utils/swalert.class';
+import Swal from 'sweetalert2';
+import { IonButton, IonIcon, IonContent } from '@ionic/angular/standalone';
+import { Cliente } from 'src/app/classes/cliente';
+import { ClienteService } from 'src/app/services/cliente.service';
 
 @Component({
   selector: 'app-formulario-cliente',
   templateUrl: './formulario-cliente.component.html',
   styleUrls: ['./formulario-cliente.component.scss'],
   standalone: true,
+  imports: [IonButton, IonIcon, IonContent, ReactiveFormsModule]
 })
 export class FormularioClienteComponent implements OnInit {
-  constructor() {}
+  formRegistrar!: FormGroup;
+  mensaje: string = 'Recordá completar todos los campos correctamente.';
 
-  ngOnInit() {
+  get nombre() {
+    return this.formRegistrar.get('nombre') as FormControl;
+  }
+  get apellido() {
+    return this.formRegistrar.get('apellido') as FormControl;
+  }
+  get dni() {
+    return this.formRegistrar.get('dni') as FormControl;
+  }
+  get cuil() {
+    return this.formRegistrar.get('cuil') as FormControl;
+  }
+  get foto() {
+    return this.formRegistrar.get('foto') as FormControl;
+  }
+  get correo() {
+    return this.formRegistrar.get('correo') as FormControl;
+  }
+  get clave() {
+    return this.formRegistrar.get('clave') as FormControl;
+  }
+
+  constructor(private clienteService: ClienteService) {
+    this.crearFormGroup();
+  }
+
+  private crearFormGroup() {
+    this.formRegistrar = new FormGroup({
+      nombre: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(20),
+        this.validarPalabra(),
+      ]),
+      apellido: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(20),
+        this.validarPalabra(),
+      ]),
+      dni: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(/^\d+$/),
+        Validators.minLength(7),
+        Validators.maxLength(9),
+      ]),
+      foto: new FormControl(undefined, [Validators.required]),
+      correo: new FormControl(null, [
+        Validators.required,
+        Validators.email,
+        Validators.required,
+      ]),
+      clave: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
+  }
+  private getCliente() {
+    let cliente = new Cliente();
+    cliente.setNombre(this.nombre.value);
+    cliente.setApellido(this.apellido.value);
+    cliente.setDni(this.dni.value);
+    cliente.setFile(this.foto.value);
+    cliente.setCorreo(this.correo.value);
+    cliente.setClave(this.clave.value);
+    return cliente;
+  }
+
+  public ngOnInit() {
     console.log('');
+  }
+
+  public async tomarFoto() {
+    try {
+      const image = await Camera.getPhoto({
+        promptLabelHeader: 'Foto de Cliente',
+        promptLabelPhoto: 'Buscar foto local',
+        promptLabelPicture: 'Tomar foto',
+        quality: 90,
+        allowEditing: false,
+        resultType: CameraResultType.Base64,
+      });
+      if (image.base64String === undefined) {
+        throw new Error('No se pudo recuperar la imagen de Camera.getPhoto()');
+      }
+
+      const mimeType = image.format === 'jpeg' ? 'image/jpeg' : 'image/png';
+      const swalertResult = await Swalert.modalCargarFoto();
+      if (swalertResult.isConfirmed) {
+        this.foto.setValue({ base64String: image.base64String, mimeType });
+      }
+    } catch (e: any) {
+      console.log(e.message);
+    } finally {
+      console.log(this.foto.value);
+    }
+  }
+  public async registrar() {
+    try {
+      const cliente = this.getCliente();
+      cliente.file = this.foto.value; // Pasa todo el objeto foto
+      await this.clienteService.alta(cliente);
+      console.log(cliente);
+      await Swalert.toastSuccess('Registrado exitosamente');
+      this.limpiar();
+    } catch (error: any) {
+      console.error('Error al registrar:', error);
+
+      if (error && error.code === 'auth/email-already-in-use') {
+        console.log('Error al registrar usuario:', error);
+      } else {
+        this.mensaje = 'Error al registrar usuario';
+      }
+    }
+  }
+  public limpiar() {
+    this.formRegistrar.reset();
+    this.nombre.setValue('');
+    this.apellido.setValue('');
+    this.dni.setValue(0);
+    this.cuil.setValue(0);
+    this.foto.setValue(undefined);
+    this.correo.setValue('');
+    this.clave.setValue('');
+  }
+
+  private validarPalabra(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const valid =
+        /^[a-zA-ZáéíóúÁÉÍÓÚñÑ'’-]+( [a-zA-ZáéíóúÁÉÍÓÚñÑ'’-]+)*$/.test(
+          control.value
+        );
+      return valid ? null : { invalidName: true };
+    };
   }
 }
--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -1,9 +1,108 @@
 import { Injectable } from '@angular/core';
+import { AuthService } from './firebase/auth.service';
+import { FirestoreService } from './firebase/firestore.service';
+import { CloudStorageService } from './firebase/cloud-storage.service';
+import { Cliente } from '../classes/cliente';
+import { firstValueFrom, isObservable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
+  private col = 'duenios';
+  private carpeta = 'duenios';
+
+  constructor(
+    private authService: AuthService,
+    private firestoreService: FirestoreService,
+    private cloudStorageService: CloudStorageService
+  ) {}
+
+  private traerProximoId() {
+    return this.firestoreService.traerProximoId(this.col, 'id');
+  }
+  
+  private async registrarAuth(cliente: Cliente) {
+    try {
+      await this.authService.registrar(cliente.correo, cliente.clave);
+    } catch (e) {
+      throw new Error('Ya existe un usuario con ese correo');
+    }
+  }
+
+  private cerrarSesionAuth() {
+    return this.authService.cerrarSesion();
+  }
+
+  private async setId(cliente: Cliente) {
+    const id = await this.traerProximoId();
+    if (id === undefined) {
+      throw new Error('El ID fue null');
+    }
+    cliente.id = id;
+  }
+
+  private async insertarFoto(cliente: Cliente) {
+    const nombreArchivo = cliente.id.toString();
+
+    await this.cloudStorageService.subirArchivoUri(
+      this.carpeta,
+      nombreArchivo,
+      cliente.file
+    );
+
+    const fotoUrl = await this.cloudStorageService.traerUrlPorNombre(
+      nombreArchivo,
+      this.carpeta
+    );
+    if (fotoUrl === undefined) {
+      throw new Error('Hubo un problema al recuperar la URL de la foto');
+    }
+    cliente.foto = fotoUrl;
+  }
+
+  private async insertarDoc(cliente: Cliente) {
+    const doc = Cliente.toDoc(cliente);
+    return this.firestoreService.insertarConId(doc.id, doc, this.col);
+  }
+
+  public async alta(cliente: Cliente) {
+    try {
+      await this.registrarAuth(cliente);
+      await this.cerrarSesionAuth();
+      await this.setId(cliente);
+      await this.insertarFoto(cliente); // !OJO! el file que se le asigna a la entidad debe ser [Uri]
+      await this.insertarDoc(cliente);
+    } catch (e: any) {
+      await this.cerrarSesionAuth();
+      throw new Error(e.message);
+    }
+  }
+
+  public traerTodosObservable() {
+    return this.firestoreService.traerTodos(this.col);
+  }
+
+  public async traerTodosPromise() {
+    const clienteObservable = this.traerTodosObservable();
+    if (isObservable(clienteObservable)) {
+      return firstValueFrom(clienteObservable);
+    }
+
+    return undefined;
+  }
+
+  public traerPorIdObservable(cliente: Cliente) {
+    const doc = Cliente.toDoc(cliente);
+    return this.firestoreService.traerPorId(doc.id, this.col);
+  }
+  public async traerPorIdPromise(cliente: Cliente) {
+    const clienteObservable = this.traerPorIdObservable(cliente);
+    if (isObservable(clienteObservable)) {
+      return firstValueFrom(clienteObservable);
+    }
+
+    return undefined;
+  }
 }
