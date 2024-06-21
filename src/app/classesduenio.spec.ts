9cd0e96ee11064bd622334209fe3b73f468cfcac
JuanPabloDH <juanpid29@gmail.com>
2024-06-20 21:29:02 -0300

    correccion al traer foto de mesa, al intentar modificarla

--- src/app/classesduenio.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Classesduenio } from './classesduenio';
-
-describe('Classesduenio', () => {
-  it('should create an instance', () => {
-    expect(new Classesduenio()).toBeTruthy();
-  });
-});
